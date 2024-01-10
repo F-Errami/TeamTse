922c8f9403d900abcc60fd0f299ae544b18af1ca
F-Errami <71508982+F-Errami@users.noreply.github.com>
2024-01-10 14:23:10 +0100

    init

---
+++ src/main/java/org/tse/teamTse/TeamTseApplication.java
@@ -0,0 +1,13 @@
+package org.tse.teamTse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTseApplication.class, args);
+	}
+
+}
